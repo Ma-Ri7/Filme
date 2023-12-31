503511e6765e776d09a98a9b3da22d1e1373e7ca
Bratu Marian <114345694+Ma-Ri7@users.noreply.github.com>
2023-12-31 15:50:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "70edb828a7ff8c923a04f39c43e2b07a",
+  VITE_CORS_PROXY_URL: "https://ma-ri7.github.io/Filme",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "https://ma-ri7.github.io/Filme",
+  VITE_TMDB_READ_API_KEY: "70edb828a7ff8c923a04f39c43e2b07a",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
