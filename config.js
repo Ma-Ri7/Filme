c7bc3d6025e26c83dd547d755116d74dd895e705
Bratu Marian <114345694+Ma-Ri7@users.noreply.github.com>
2023-12-31 15:52:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "https://ma-ri7.github.io/Filme",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "70edb828a7ff8c923a04f39c43e2b07a",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGVkYjgyOGE3ZmY4YzkyM2EwNGYzOWM0M2UyYjA3YSIsInN1YiI6IjY1OTE2NTdmY2U0ZGRjNmQzODdkZTc4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bk8v-osdfmfilg4Wy7mI-Ui-OkoljDV2n8y9B3HSIrk",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
